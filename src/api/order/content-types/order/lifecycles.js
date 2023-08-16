a1b838ad49261b49b6e1bc8751c4f7e49cf44edd
Johannes Ull <jull.development@gmail.com>
2023-08-16 20:35:10 +0200

    fix sendgrid

--- src/api/order/content-types/order/lifecycles.js
+++ src/api/order/content-types/order/lifecycles.js
@@ -1,16 +1,19 @@
 module.exports = {
   async afterCreate(event) {
-    console.log("Created an event: ", event);
-
     const { result } = event;
 
+    // console.log("Created an event: ", result);
+    //console.log(strapi.plugins["email"].services.email);
+
     try {
-      await strapi.plugins["email"].services.email.send({
+      const res = await strapi.plugins["email"].services.email.send({
         to: result.customer_email,
         from: "contact@stadium-avenue.com",
         subject: "Your order at stadium avenue",
         text: `Thanks for your purchase! Here is your unique order identifier: ${result.order_id}. You can use this ID to track your order at any time.`,
       });
+
+      console.log("Sent email: ", res);
     } catch (error) {
       console.log("error", error);
     }

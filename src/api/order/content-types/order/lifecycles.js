6bf248725075462d4c25be3c315074253818e38d
Johannes Ull <jull.development@gmail.com>
2023-08-16 21:02:41 +0200

    add uuid

--- src/api/order/content-types/order/lifecycles.js
+++ src/api/order/content-types/order/lifecycles.js
@@ -2,18 +2,13 @@ module.exports = {
   async afterCreate(event) {
     const { result } = event;
 
-    console.log("Created an event: ", result);
-    console.log(strapi.plugins["email"].services.email);
-
     try {
       const res = await strapi.plugins["email"].services.email.send({
         to: result.customer_email,
         from: "contact@stadium-avenue.com",
         subject: "Your order at stadium avenue",
-        text: `Thanks for your purchase! Here is your unique order identifier: ${result.id}. You can use this ID to track your order at any time.`,
+        text: `Thanks for your purchase! Here is your unique order identifier: ${result.order_id}. You can use this ID to track your order at any time.`,
       });
-
-      console.log("Sent email: ", res);
     } catch (error) {
       console.log("error", error);
     }
--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,21 +1,25 @@
 "use strict";
 
+const { v4: uuidv4 } = require("uuid");
+
 /**
  * order controller
  */
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::order.order");
+//module.exports = createCoreController('api::order.order');
 
-/* module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
   // wrap a core action, leaving core logic in place
   async create(ctx) {
-    // some logic here
-    console.log("ctx.request.body", ctx.request.body);
+    const uuid = uuidv4();
+
+    ctx.request.body.data.order_id = uuid;
+
     const response = await super.create(ctx);
     // some more logic
 
     return response;
   },
-})); */
+}));
